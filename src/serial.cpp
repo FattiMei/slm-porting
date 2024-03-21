deb3790190e2602925bc13cad787fcbad3eec9fe
FattiMei <teomei68@gmail.com>
2024-03-21 19:20:33 +0100

    Refactored rs kernel to expose the original algorithm

--- src/serial.cpp
+++ src/serial.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include <cmath>
 #include <complex>
+#include <cassert>
 
 
 SLM::SLM(int width_, int height_, double wavelength_um_, double pixel_size_um_, double focal_length_mm_) : par(width_, height_, focal_length_mm_, pixel_size_um_, wavelength_um_), phase_buffer(width_ * height_), texture_buffer(width_ * height_) {
@@ -27,6 +28,7 @@ void SLM::rs_kernel(int n, const Point3D spots[], double phase[], const SLMParam
 	const int    &HEIGHT       = par->height;
 	const double &FOCAL_LENGTH = par->focal_length_mm;
 	const double &WAVELENGTH   = par->wavelength_um;
+	const std::complex<double> IOTA(0.0, 1.0);
 
 
 	// random pist generation
@@ -41,45 +43,29 @@ void SLM::rs_kernel(int n, const Point3D spots[], double phase[], const SLMParam
 		}
 	}
 
-	// pupil coords generation
-	std::vector<double> pupil_x;
-	std::vector<double> pupil_y;
-	std::vector<int> pupil_index;
 
-	// @OPT: make the process of selecting pupil points inside the loop below
-	{
-		// careful in how you iterate over the two indices
-		// @POSSIBLE_BUG: look at the iteration space!
-		for (int i = 0; i < WIDTH; ++i) {
-			for (int j = 0; j < WIDTH; ++j) {
-				// @TODO: find the correct implementation of linspace
-				const double x_cand = -1.0 + static_cast<double>(i) * 2.0 / static_cast<double>(WIDTH);
-				const double y_cand = -1.0 + static_cast<double>(j) * 2.0 / static_cast<double>(HEIGHT);
+	// @BUG: look carefully at the iteration space, are we storing column major or row major?
+	for (int i = 0; i < WIDTH; ++i) {
+		for (int j = 0; j < HEIGHT; ++j) {
+			// @TODO: see correct linspace implementation
+			double x = -1.0 + static_cast<double>(i) * 2.0 / static_cast<double>(WIDTH);
+			double y = -1.0 + static_cast<double>(j) * 2.0 / static_cast<double>(HEIGHT);
 
-				if (x_cand*x_cand + y_cand*y_cand < 1.0) {
-					pupil_index.push_back(i * WIDTH + j);
+			if (x*x + y*y < 1.0) {
+				std::complex<double> total_field(0.0, 0.0);
+				x = x * FOCAL_LENGTH * static_cast<double>(WIDTH) / 2.0;
+				y = y * FOCAL_LENGTH * static_cast<double>(WIDTH) / 2.0;
 
-					pupil_x.push_back(x_cand * FOCAL_LENGTH * static_cast<double>(WIDTH) / 2.0);
-					pupil_y.push_back(y_cand * FOCAL_LENGTH * static_cast<double>(HEIGHT) / 2.0);
+				// @TODO: find a better name for this iteration variable, we are iterating over the spots
+				for (int k = 0; k < n; ++k) {
+					const double p_phase = 2.0 * M_PI / (WAVELENGTH * FOCAL_LENGTH * 1000.0) * (spots[k].x * x + spots[k].y * y) + M_PI * spots[k].z / (WAVELENGTH * FOCAL_LENGTH * FOCAL_LENGTH * 1e6) * (x * x + y * y);
+
+					total_field += std::exp(IOTA * (p_phase + pists[k]));
 				}
+
+				phase[i * WIDTH + j] = std::arg(total_field);
 			}
 		}
-	}
-
-
-	// thinking in the reverse order seems helpful
-	const std::complex<double> IOTA(0.0, 1.0);
-
-	for (size_t i = 0; i < pupil_index.size(); ++i) {
-		std::complex<double> total_field(0.0, 0.0);
-
-		for (int j = 0; j < n; ++j) {
-			const double p_phase = 2.0 * M_PI / (WAVELENGTH * FOCAL_LENGTH * 1000.0) * (spots[j].x * pupil_x[i] + spots[j].y * pupil_y[i]) + M_PI * spots[j].z / (WAVELENGTH * FOCAL_LENGTH * FOCAL_LENGTH * 1e6) * (pupil_x[i] * pupil_x[i] + pupil_y[i] * pupil_y[i]);
-
-			total_field += std::exp(IOTA * (p_phase + pists[j]));
-		}
-
-		phase[pupil_index[i]] = std::arg(total_field / static_cast<double>(pupil_index.size()));
 	}
 }
 
