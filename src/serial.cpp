6b71a5c83af26c99b0561bdabd92a0906a9a8938
FattiMei <teomei68@gmail.com>
2024-03-23 11:17:17 +0100

    Removed permanently inefficient kernels

--- src/serial.cpp
+++ src/serial.cpp
@@ -21,32 +21,6 @@ void generate_random_pistons(std::vector<double> &pists, int seed) {
 }
 
 
-struct Point2D {
-	double x;
-	double y;
-
-	Point2D(double x_, double y_) : x(x_), y(y_) {};
-};
-
-
-void filter_pupil_points(std::vector<Point2D> &pupil_points, std::vector<int> &pupil_index, double focal_length, int width, int height) {
-	for (int j = 0; j < height; ++j) {
-		for (int i = 0; i < width; ++i) {
-			double x = linspace(-1.0, 1.0, width,  i);
-			double y = linspace(-1.0, 1.0, height, j);
-
-			if (x*x + y*y < 1.0) {
-				pupil_index.push_back(j * width + i);
-
-				x = x * focal_length * static_cast<double>(width) / 2.0;
-				y = y * focal_length * static_cast<double>(height) / 2.0;
-
-				pupil_points.emplace_back(x, y);
-			}
-		}
-	}
-}
-
 SLM::SLM(int width_, int height_, double wavelength_um_, double pixel_size_um_, double focal_length_mm_) : par(width_, height_, focal_length_mm_, pixel_size_um_, wavelength_um_), phase_buffer(width_ * height_), texture_buffer(width_ * height_) {
 }
 
@@ -92,34 +66,6 @@ void SLM::rs_kernel(int n, const Point3D spots[], double pists[], double phase[]
 				phase[j * WIDTH + i] = std::arg(total_field);
 			}
 		}
-	}
-}
-
-
-void SLM::rs_kernel_inefficient(int n, const Point3D spots[], double pists[], double phase[], const SLMParameters *par, Performance *perf) {
-	(void) perf;
-	const int    &WIDTH        = par->width;
-	const int    &HEIGHT       = par->height;
-	const double &FOCAL_LENGTH = par->focal_length_mm;
-	const double &WAVELENGTH   = par->wavelength_um;
-	const std::complex<double> IOTA(0.0, 1.0);
-
-
-	std::vector<Point2D> pupil_points;
-	std::vector<int> pupil_index;
-	filter_pupil_points(pupil_points, pupil_index, FOCAL_LENGTH, WIDTH, HEIGHT);
-
-
-	for (size_t i = 0; i < pupil_index.size(); ++i) {
-		std::complex<double> total_field(0.0, 0.0);
-
-		for (int j = 0; j < n; ++j) {
-			const double p_phase = 2.0 * M_PI / (WAVELENGTH * FOCAL_LENGTH * 1000.0) * (spots[j].x * pupil_points[i].x + spots[j].y * pupil_points[i].y) + M_PI * spots[j].z / (WAVELENGTH * FOCAL_LENGTH * FOCAL_LENGTH * 1e6) * (pupil_points[i].x * pupil_points[i].x + pupil_points[i].y * pupil_points[i].y);
-
-			total_field += std::exp(IOTA * (p_phase + pists[j]));
-		}
-
-		phase[pupil_index[i]] = std::arg(total_field / static_cast<double>(pupil_index.size()));
 	}
 }
 
@@ -178,7 +124,7 @@ void SLM::rs(const std::vector<Point3D> &spots, int seed, bool measure) {
 	std::vector<double> pists(spots.size());
 	generate_random_pistons(pists, seed);
 
-	rs_kernel_inefficient(spots.size(), spots.data(), pists.data(), phase_buffer.data(), &par, measure ? &perf : NULL);
+	rs_kernel(spots.size(), spots.data(), pists.data(), phase_buffer.data(), &par, measure ? &perf : NULL);
 }
 
 
