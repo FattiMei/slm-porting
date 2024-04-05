4e6553c065216ad6127d6dc445208f4c998815ad
FattiMei <teomei68@gmail.com>
2024-04-05 18:37:33 +0200

    Refactor kernel invocations in main.cpp for testing with python

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <random>
+#include <complex>
 #include "slm.hpp"
 #include "utils.hpp"
 #include "units.hpp"
+#include "kernels.hpp"
 
 
 const int width            = 512;
@@ -20,8 +22,6 @@ const Length focal_length( 20.0, Unit::Millimeters);
 
 
 int main(int argc, char *argv[]) {
-	(void) argv;
-
 	if (argc != 2) {
 		std::cerr << "Error in command line arguments" << std::endl;
 		std::cerr << "Usage: porting <output_filename>" << std::endl;
@@ -29,29 +29,32 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	/*
-	SLM slm(width, height, wavelength, pitch, focal_length);
+
+	const SLM::Parameters parameters(width, height, focal_length, pitch, wavelength);
+
+	const std::vector<Point3D> spots = generate_grid_spots(10, 10.0);
+	const int nspots = spots.size();
+
+	std::vector<double> pists = generate_random_vector(nspots, 0.0, 2.0 * M_PI, 1);
+	std::vector<double> pists_tmp(nspots);
+	std::vector<double> weights(nspots);
+	std::vector<double> phase(parameters.width * parameters.height);
+	std::vector<double> ints(nspots);
+	std::vector<std::complex<double>> spot_fields;
+
+
 	std::ofstream out(argv[1]);
 
 
-	std::vector<double> pists = generate_random_vector(npoints, 0.0, 2.0 * M_PI, 1);
+	rs_kernel(nspots, spots.data(), pists.data(), phase.data(), &parameters, NULL);
+	// gs_kernel(nspots, spots.data(), pists.data(), pists_tmp.data(), spot_fields.data(), phase.data(), &parameters, NULL, iterations);
+	// wgs_kernel(nspots, spots.data(), pists.data(), pists_tmp.data(), spot_fields.data(), ints.data(), weights.data(), phase.data(), &parameters, NULL, iterations);
+	// csgs_kernel(nspots, spots.data(), pists.data(), pists_tmp.data(), spot_fields.data(), phase.data(), &parameters, NULL, iterations, compression, seed);
+	// wcsgs_kernel(nspots, spots.data(), pists.data(), pists_tmp.data(), spot_fields.data(), ints.data(), weights.data(), phase.data(), &parameters, NULL, iterations, compression, seed);
 
-
-	std::vector<Point3D> spots;
-	generate_grid_spots(10, 10.0, spots);
-
-
-	// slm.rs(spots, pists);
-	// slm.gs(spots, pists, iterations);
-	// slm.wgs(spots, pists, iterations);
-	// slm.csgs(spots, pists, iterations, compression);
-	slm.wcsgs(spots, pists, iterations, compression);
-
-
-	slm.write_on_file(out);
-	write_vector_on_file(pists, npoints, 1, out);
+	write_vector_on_file(phase, parameters.width, parameters.height, out);
+	write_vector_on_file(pists, nspots, 1, out);
 	write_spots_on_file(spots, out);
-	*/
 
 
 	return 0;
