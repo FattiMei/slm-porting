6a13fc83baf9c9306003eb53478b7123bc027654
FattiMei <teomei68@gmail.com>
2024-04-10 21:36:11 +0200

    Fixed bug in gs kernel, pistons computing was replicated for no reason

--- include/kernels.hpp
+++ include/kernels.hpp
@@ -13,9 +13,7 @@ void rs_kernel_pupil_index_bounds (const int n, const Point3D spots[], const dou
 void rs_kernel_static_index_bounds(const int n, const Point3D spots[], const double pists[], double phase[],                                                                       const SLM::Parameters *par);
 
 
-// optimizations related to rescheduling mathematical operations (might be different due to finite precision)
-void gs_kernel_naive      (const int n, const Point3D spots[], double pists[], double pists_copy_buffer[], std::complex<double> spot_fields[], double phase[], const SLM::Parameters *par, const int iterations);
-void gs_kernel_loop_fusion(const int n, const Point3D spots[], double pists[], double pists_copy_buffer[], std::complex<double> spot_fields[], double phase[], const SLM::Parameters *par, const int iterations);
+void gs_kernel_naive(const int n, const Point3D spots[], double pists[], std::complex<double> spot_fields[], double phase[], const SLM::Parameters *par, const int iterations);
 
 
 void wgs_kernel(
