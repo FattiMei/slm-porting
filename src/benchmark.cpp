c7d03de9fb6d418f795ba2ec193366c858d5bff2
FattiMei <teomei68@gmail.com>
2024-04-14 10:28:02 +0200

    refactor: benchmarking code takes lambdas that capture the differences between kernel implementations

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <chrono>
+#include <string>
+#include <complex>
 #include "slm.hpp"
 #include "utils.hpp"
 #include "kernels.hpp"
@@ -9,7 +11,23 @@
 #define SEED     1
 
 
-// we don't care about correctness in this experiment
+template <typename Run>
+void benchmark(int samples, const std::string &name, Run run_cmd) {
+	std::cout << name << std::endl;
+
+	for (int i = 0; i < samples; ++i) {
+		const auto start_time = std::chrono::high_resolution_clock::now();
+
+		run_cmd();
+
+		const auto end_time = std::chrono::high_resolution_clock::now();
+		const auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+
+		std::cout << delta << std::endl;
+	}
+}
+
+
 int main() {
 	SLM::Parameters parameters(
 		512,
@@ -19,63 +37,60 @@ int main() {
 		Length(488.0, Unit::Nanometers)
 	);
 
-	const auto spots              = generate_grid_spots(10, 10.0);
-	const auto pists              = generate_random_vector(spots.size(), 0.0, 2.0 * M_PI, 1);
+	const auto spots = generate_grid_spots(10, 10.0);
+	const int  N	 = spots.size();
+	auto pists = generate_random_vector(N, 0.0, 2.0 * M_PI, 1);
+
+	// specific for rs testing
 	const auto pupil_indices      = generate_pupil_indices(parameters);
 	const auto pupil_index_bounds = generate_pupil_index_bounds(parameters);
+
+	// specific for gs testing
+	std::vector<std::complex<double>> spot_fields(N);
+	std::vector<double> p_phase_cache(N);
+
 	std::vector<double> phase(parameters.width * parameters.height);
 
 
-	std::cout << "# rs kernel naive (ms)" << std::endl;
-	for (int i = 0; i < NSAMPLES; ++i) {
-		const auto start_time = std::chrono::high_resolution_clock::now();
+	const auto rs_naive_invocation = [&] {
+		rs_kernel_naive(N, spots.data(), pists.data(), phase.data(), &parameters);
+	};
 
-		rs_kernel_naive(spots.size(), spots.data(), pists.data(), phase.data(), &parameters);
+	const auto rs_pupil_indices_invocation = [&] {
+		rs_kernel_pupil_indices(N, spots.data(), pists.data(), phase.data(), pupil_indices.size(), pupil_indices.data(), &parameters);
+	};
 
-		const auto end_time = std::chrono::high_resolution_clock::now();
-		const auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+	const auto rs_pupil_index_bounds_invocation = [&] {
+		rs_kernel_pupil_index_bounds(N, spots.data(), pists.data(), phase.data(), pupil_index_bounds.data(), &parameters);
+	};
 
-		std::cout << delta << std::endl;
-	}
+	const auto rs_static_index_bounds_invocation = [&] {
+		rs_kernel_static_index_bounds(N, spots.data(), pists.data(), phase.data(), &parameters);
+	};
 
+	// i know that these invocations will modify pists data, it shouldn't be a problem since here we are testing only performance and the algorithms are static
+	const auto gs_naive_invocation = [&] {
+		gs_kernel_naive(N, spots.data(), pists.data(), spot_fields.data(), phase.data(), &parameters, 30);
+	};
 
-	std::cout << "# rs kernel precomputed pupil indices (ms)" << std::endl;
-	for (int i = 0; i < NSAMPLES; ++i) {
-		const auto start_time = std::chrono::high_resolution_clock::now();
+	const auto gs_cached_invocation = [&] {
+		gs_kernel_cached(N, spots.data(), pists.data(), p_phase_cache.data(), spot_fields.data(), phase.data(), &parameters, 30);
+	};
 
-		rs_kernel_pupil_indices(spots.size(), spots.data(), pists.data(), phase.data(), pupil_indices.size(), pupil_indices.data(), &parameters);
-
-		const auto end_time = std::chrono::high_resolution_clock::now();
-		const auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-
-		std::cout << delta << std::endl;
-	}
-
-
-	std::cout << "# rs kernel precomputed pupil index bounds (ms)" << std::endl;
-	for (int i = 0; i < NSAMPLES; ++i) {
-		const auto start_time = std::chrono::high_resolution_clock::now();
-
-		rs_kernel_pupil_index_bounds(spots.size(), spots.data(), pists.data(), phase.data(), pupil_index_bounds.data(), &parameters);
-
-		const auto end_time = std::chrono::high_resolution_clock::now();
-		const auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-
-		std::cout << delta << std::endl;
-	}
+	const auto gs_reordered_invocation = [&] {
+		gs_kernel_reordered(N, spots.data(), pists.data(), spot_fields.data(), phase.data(), &parameters, 30);
+	};
 
 
-	std::cout << "# rs kernel runtime computed pupil index bounds (ms)" << std::endl;
-	for (int i = 0; i < NSAMPLES; ++i) {
-		const auto start_time = std::chrono::high_resolution_clock::now();
+	benchmark(NSAMPLES, "rs naive",				rs_naive_invocation);
+	benchmark(NSAMPLES, "rs precomputed pupil indices",	rs_pupil_indices_invocation);
+	benchmark(NSAMPLES, "rs precomputed index bounds",	rs_pupil_index_bounds_invocation);
+	benchmark(NSAMPLES, "rs runtime computed index bounds", rs_static_index_bounds_invocation);
 
-		rs_kernel_static_index_bounds(spots.size(), spots.data(), pists.data(), phase.data(), &parameters);
+	benchmark(1, "gs naive",	gs_naive_invocation);
+	benchmark(1, "gs cached", 	gs_cached_invocation);
+	benchmark(1, "gs reordered",	gs_reordered_invocation);
 
-		const auto end_time = std::chrono::high_resolution_clock::now();
-		const auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-
-		std::cout << delta << std::endl;
-	}
 
 	return 0;
 }
