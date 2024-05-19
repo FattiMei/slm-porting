979ee1d07059be10f46fc592d9529f94fe52eddf
FattiMei <teomei68@gmail.com>
2024-05-19 18:45:52 +0200

    Add rs precomputed indices benchmark

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -37,7 +37,23 @@ static void BM_rs_branchless(benchmark::State &state) {
 }
 
 
+static void BM_rs_indices(benchmark::State &state) {
+	const auto spots = generate_grid_spots(10, 10.0);
+	const int  N	 = spots.size();
+	auto pists = generate_random_vector(N, 0.0, 2.0 * M_PI, 1);
+	std::vector<double> phase(parameters.width * parameters.height);
+
+	auto pupil_indices = generate_pupil_indices(parameters);
+
+
+	for (auto _ : state) {
+		rs_kernel_pupil_indices(N, spots.data(), pists.data(), phase.data(), pupil_indices.size(), pupil_indices.data(), &parameters);
+	}
+}
+
+
 // @TODO: set the time unit in the command invocation
 BENCHMARK(BM_rs_branchless)->Unit(benchmark::kMillisecond);
 BENCHMARK(BM_rs_naive)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_rs_indices)->Unit(benchmark::kMillisecond);
 BENCHMARK_MAIN();
