f46da42b152308d1bed51199491d007f4512b63a
FattiMei <teomei68@gmail.com>
2024-05-21 08:28:49 +0200

    Complete custom thread scheduling generator

--- python/scheduling.py
+++ python/scheduling.py
@@ -1,18 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 RESOLUTION      = 512
 OMP_NUM_THREADS = 4
 
 
+# lame search but it works
 def reverse_search(ordered, x):
-    # lame search
-    for i in range(len(ordered)):
-        if x < ordered[i]:
-            break
+    for i in range(len(ordered) - 1):
+        if x >= ordered[i] and x < ordered[i+1]:
+            return i
 
-    return i
+    return len(ordered)
+
+
+def format_bounds(bounds):
+    print('const int for_loop_bounds[] = {')
+
+    for x in bounds:
+        print(f'\t{x},')
+
+    print('};')
 
 
 if __name__ == '__main__':
@@ -21,14 +29,11 @@ if __name__ == '__main__':
         np.linspace(-1, 1, RESOLUTION)
     )
 
-    points_in_line = np.sum((xx ** 2 + yy ** 2) < 1.0, axis=0)
-    cumulative     = np.cumsum(points_in_line)
-    cumulative     = cumulative / cumulative[-1]
+    points_in_line = np.sum((xx ** 2 + yy ** 2) < 1.0, axis=1)
+    cumulative     = np.cumsum(points_in_line, dtype = np.float64)
 
+    print(cumulative)
+    print(np.linspace(cumulative[0], cumulative[-1], OMP_NUM_THREADS + 1))
 
-    bounds = [reverse_search(cumulative, alpha) for alpha in np.linspace(0, 1, OMP_NUM_THREADS + 1)]
-    print(bounds)
-
-
-    plt.plot(cumulative)
-    plt.show()
+    bounds = [reverse_search(cumulative, alpha) for alpha in np.linspace(cumulative[0], cumulative[-1], OMP_NUM_THREADS + 1)]
+    format_bounds(bounds)
