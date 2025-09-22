d9496a8ad32eb1a8150ab839806812052b4bc112
FattiMei <teomei68@gmail.com>
2025-09-22 18:38:32 +0200

    Add complete RNG suite for jax compatibility, with tests

--- python/rng.py
+++ python/rng.py
@@ -1,5 +1,9 @@
 import copy
 import unittest
+
+import jax
+import jax.numpy as jnp
+jax.config.update('jax_enable_x64', True)
 import numpy as np
 
 
@@ -8,59 +12,145 @@ Requirements for a Random Number Generator class:
     * gather multiple rng implementations under one interface (e.g both numpy and jax)
     * test the performance of rng implementations
     * obtain reproducible results in non-deterministic algorithms
+the latter can be accomplished by the `clone` method
 
-the latter can be accomplished by cloning an RNG or by supplying a constant number
+The critical class is the `JaxWrapperRng` because it makes possible to test jax
+implementations (which require their own rng to be compiled efficiently) with the
+other ones:
+    1. the jax implementation is run with a PRNG key
+    2. the same key is used to construct a stateful RNG
+    3. the other implementations are tested using clones of this stateful RNG
+
+if the jax and non-jax implementations share the same calls to the RNG, they will
+roll the same exact numbers. This behaviour is tested below
 '''
 class Rng:
-    def __init__(self, seed: int):
-        pass
+    def sample(self, shape, dtype):
+        raise NotImplementedError
 
-    def sample(self, size: int):
-        pass
+    def clone(self):
+        raise NotImplementedError
 
 
 class NumpyRng(Rng):
-    def __init__(self, seed: int):
+    def __init__(self, seed):
         self.rng = np.random.default_rng(seed)
 
-    def sample(self, size: int, dtype=np.float64):
-        return self.rng.random(size, dtype=dtype)
+    def sample(self, shape=1, dtype=np.float64):
+        return self.rng.random(shape, dtype=dtype)
+
+    def clone(self):
+        return copy.deepcopy(self)
 
 
 class ConstRng(Rng):
     def __init__(self, constant: float):
         self.constant = constant
 
-    def sample(self, size: int, dtype=np.float64):
-        return self.constant * np.ones(size, dtype=dtype)
+    def sample(self, shape, dtype=np.float64):
+        return self.constant * np.ones(shape, dtype=dtype)
+
+    def clone(self):
+        return ConstRng(self.constant)
 
 
-class TestRng(unittest.TestCase):
-    def test_numpy(self):
-        rng = NumpyRng(42)
+class JaxWrapperRng(Rng):
+    def __init__(self, key):
+        self.key = key
 
-        for size in [1, 2, 10, 100]:
-            x = rng.sample(size)
-            self.assertTrue(x.shape == (size,))
+    def sample(self, shape=1, dtype=None):
+        result = jax.random.uniform(
+            self.key,
+            shape=shape,
+            dtype=dtype
+        )
 
-    # this is the example for cloning a rng
-    def test_numpy_cloning(self):
-        rng = NumpyRng(69)
-        _ = rng.sample(100)
-        clone = copy.deepcopy(rng)
+        # this makes the class stateful
+        self.key, _ = jax.random.split(self.key)
 
-        size = 100
-        self.assertTrue(np.all(rng.sample(size) == clone.sample(size)))
+        return result
 
-    def test_constant_type(self):
-        rng = ConstRng(constant=3.14)
-        size = 100
+    def clone(self):
+        return JaxWrapperRng(self.key)
 
-        for dtype in [np.float16, np.float32, np.float64, np.float128]:
-            x = rng.sample(size, dtype)
 
-            self.assertTrue(x.shape == (size,))
-            self.assertTrue(x.dtype == dtype)
+class TestNumpyRng(unittest.TestCase):
+    def test_type_promotion(self):
+        rng = NumpyRng(seed=42)
+
+        for dtype in [np.float32, np.float64]:
+            with self.subTest(dtype=dtype):
+                pists = 2.0 * dtype(np.pi) * rng.sample(shape=(1,), dtype=dtype)
+                self.assertTrue(pists.dtype == dtype)
+
+    def test_clone(self):
+        SHAPE = (100,)
+
+        rng = NumpyRng(seed=42)
+        clone = rng.clone()
+
+        self.assertTrue(np.allclose(
+            rng.sample(SHAPE),
+            clone.sample(SHAPE),
+            atol=0.0
+        ))
+
+    def test_use_after_clone(self):
+        SHAPE = (100,)
+
+        rng = NumpyRng(seed=42)
+        _ = rng.sample()
+        clone = rng.clone()
+
+        self.assertTrue(np.allclose(
+            rng.sample(SHAPE),
+            clone.sample(SHAPE),
+            atol=0.0
+        ))
+
+
+class TestJaxWrapperRng(unittest.TestCase):
+    def test_type_promotion(self):
+        rng = JaxWrapperRng(jax.random.key(42))
+
+        for dtype in [jnp.float16, jnp.bfloat16, jnp.float32, jnp.float64]:
+            with self.subTest(dtype=dtype):
+                pists = 2.0 * jnp.pi * rng.sample(shape=(1,), dtype=dtype)
+                self.assertTrue(pists.dtype == dtype)
+
+    def test_statefulness(self):
+        SHAPE = (100,)
+        rng = JaxWrapperRng(jax.random.key(42))
+
+        first = rng.sample(SHAPE)
+        second = rng.sample(SHAPE)
+
+        self.assertFalse(jnp.allclose(first, second, atol=0.0))
+
+    def test_clone(self):
+        SHAPE = (100,)
+
+        rng = JaxWrapperRng(jax.random.key(42))
+        clone = rng.clone()
+
+        self.assertTrue(jnp.allclose(
+            rng.sample(SHAPE),
+            clone.sample(SHAPE),
+            atol=0.0
+        ))
+
+    def test_use_after_clone(self):
+        SHAPE = (100,)
+
+        rng = JaxWrapperRng(jax.random.key(42))
+        _ = rng.sample()
+        clone = rng.clone()
+
+        self.assertTrue(jnp.allclose(
+            rng.sample(SHAPE),
+            clone.sample(SHAPE),
+            atol=0.0
+        ))
 
 
 if __name__ == '__main__':
