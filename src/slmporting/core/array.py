c20f9c11093fe53f73d238e61d3ca285267c4a27
FattiMei <teomei68@gmail.com>
2025-10-04 17:06:39 +0200

    Add generic array implementation with conversion from an to every backend

--- src/slmporting/core/array.py
+++ src/slmporting/core/array.py
@@ -1,5 +1,7 @@
 import jax
+import jax.numpy as jnp
 import torch
+import torch.utils.dlpack
 import numpy as np
 
 from slmporting.core.types import Backend, Locality, DType
@@ -19,9 +21,92 @@ jax_dtype_map = {
 }
 
 
+jax_device_map = {
+    Locality.CPU: 'cpu',
+    Locality.GPU: 'gpu'
+}
+
+
+torch_dtype_map = {
+    DType.fp16:  torch.float16,
+    DType.fp32:  torch.float32,
+    DType.fp64:  torch.float64
+}
+
+
+torch_device_map = {
+    Locality.CPU: 'cpu',
+    Locality.GPU: 'cuda'
+}
+
+
+def inverse_search_dict(dtype_map, value):
+    for key in dtype_map.keys():
+        if dtype_map[key] == value:
+            return key
+
+
 class Array:
-    def __init__(self, data: np.ndarray):
+    def __init__(self, data):
+        if isinstance(data, np.ndarray):
+            backend = Backend.NUMPY
+            locality = Locality.CPU
+            dtype = inverse_search_dict(numpy_dtype_map, data.dtype)
+
+        elif isinstance(data, jax.numpy.array):
+            backend = Backend.JAX
+            locality = Locality.CPU if data.device().platform == 'cpu' else Locality.GPU
+            dtype = inverse_search_dict(jax_dtype_map, data.dtype)
+
+        elif isinstance(data, torch.tensor):
+            backend = Backend.TORCH
+            locality = Locality.CPU if data.device.type == 'cpu' else Locality.GPU
+            dtype = inverse_search_dict(torch_dtype_map, data.dtype)
+
+        else:
+            assert(False)
+
         self.data = data
+        self.backend = backend
+        self.locality = locality
+        self.dtype = dtype
+
+    '''
+    returns an array of the requested type (zero-copy if possible)
+    '''
+    def convert_to(self, backend: Backend, locality: Locality, dtype: DType):
+        if backend == Backend.NUMPY:
+            assert(locality == Locality.CPU)
+            result = np.array(
+                np.from_dlpack(self.data),
+                dtype = numpy_dtype_map[dtype]
+            )
+
+        elif backend == Backend.JAX:
+            result = jnp.array(
+                jnp.from_dlpack(self.data, device=jax_device_map[locality]),
+                dtype = jax_dtype_map[dtype]
+            )
+
+        elif backend == Backend.TORCH:
+            result = torch.from_dlpack(self.data).to(
+                device = torch_device_map[locality],
+                dtype = torch_dtype_map[dtype]
+            )
+
+        else:
+            assert(False)
+
+        return result
+
+
+class CachedArray:
+    def __init__(self, data):
+        self.arr = Array(data)
+        self.cache = {
+            Locality.CPU: {},
+            Locality.GPU: {}
+        }
 
     def convert_to(
         self,
@@ -29,53 +114,11 @@ class Array:
         locality: Locality = Locality.CPU, 
         dtype: DType = DType.fp64
     ):
-        if backend == Backend.NUMPY:
-            assert(locality == Locality.CPU)
-            return np.array(self.data, dtype=numpy_dtype_map[dtype])
+        if dtype not in self.cache[locality]:
+            self.cache[locality][dtype] = self.arr.convert_to(
+                backend = Backend.TORCH,
+                locality = locality,
+                dtype = dtype
+            )
 
-        elif backend == Backend.JAX:
-            if locality == Locality.CPU:
-                return jnp.array(self.data, dtype=jax_dtype_map[dtype])
-
-            elif locality == Locality.GPU:
-                # TODO: still in progress because I can't test it on my machine
-                pass
-
-            else:
-                assert(False)
-
-        elif backend == Backend.TORCH:
-            if locality == Locality.CPU:
-                return torch.from_numpy(self.data)
-
-            elif locality == Locality.GPU:
-                # TODO: still in progress because I can't test it on my machine
-                pass
-
-            else:
-                assert(False)
-
-        else:
-            assert(False)
-
-
-class CachedArray(Array):
-    def __init__(self, data: np.ndarray):
-        super().__init__(data)
-        self.cache = {
-            Locality.CPU: {},
-            Locality.GPU: {}
-        }
-
-    def convert_to(
-        backend: Backend,
-        locality: Locality = Locality.CPU, 
-        dtype: DType = DType.fp64
-    ):
-        # TODO
-        # logica abbastanza normale, non la sviluppo adesso
-        # perché perderei solo tempo.
-        #
-        # basti pensare che questo è in grado di cachare il dato (compreso il suo dtype)
-        # ed è molto comodo per le implementazioni gpu
-        super().convert_to(backend, locality, dtype)
+        return self.cache[locality][dtype]
--- tests/unit/test_array.py
+++ tests/unit/test_array.py
@@ -21,3 +21,30 @@ def test_numpy_conversion(dtype: DType):
 
     assert(type(converted) == np.ndarray)
     assert(converted.dtype == numpy_dtype_map[dtype])
+
+
+@pytest.mark.parametrize("backend", [Backend.NUMPY, Backend.JAX, Backend.TORCH])
+@pytest.mark.parametrize("dtype", [DType.fp16, DType.fp32, DType.fp64])
+def test_caching(backend: Backend, dtype: DType):
+    arr = CachedArray(np.random.rand(100))
+
+    first = arr.convert_to(
+        backend = backend,
+        locality = Locality.CPU,
+        dtype = dtype
+    )
+
+    # this second time the array has already been
+    # cached, so there isn't a copy
+    second = arr.convert_to(
+        backend = backend,
+        locality = Locality.CPU,
+        dtype = dtype
+    )
+
+    # one would have to test that the two arrays
+    # share the same memory
+    #
+    # they are not anymore the same object because
+    # they are build on the go from a previously
+    # allocated memory
