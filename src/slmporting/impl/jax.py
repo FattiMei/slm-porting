d8d5264bc52fa002821f2faecc690f8817606ae9
FattiMei <teomei68@gmail.com>
2025-10-05 09:58:14 +0200

    Add jax implementations

---
+++ src/slmporting/impl/jax.py
@@ -0,0 +1,42 @@
+import jax
+jax.config.update('jax_enable_x64', True)
+import jax.numpy as jnp
+
+from slmporting.core.types import Algorithm, Backend, Device, DType, Tensor
+from slmporting.core.contract import impl
+
+
+ε = jnp.newaxis
+
+
+@impl(Algorithm.RS, Backend.JAX, (Device.CPU, Device.GPU), description = 'same code')
+def rs(x: Tensor, y: Tensor, z: Tensor, pists: Tensor, xx: Tensor, yy: Tensor, C1: float, C2: float):
+    return jnp.angle(
+        jnp.mean(
+            jnp.exp(
+                1j * (
+                    C1 * (x[:,ε]*xx[ε,:] + y[:,ε]*yy[ε,:]) +
+                    C2 * z[:,ε] * (xx**2 + yy**2)[ε,:] +
+                    2*jnp.pi*pists[:,ε]
+                )
+            ),
+            axis=0
+        )
+    )
+
+
+@impl(Algorithm.RS, Backend.JAX, (Device.CPU, Device.GPU), description = 'simulate complex numbers with real numbers')
+def rs_no_complex(x: Tensor, y: Tensor, z: Tensor, pists: Tensor, xx: Tensor, yy: Tensor, C1: float, C2: float):
+    slm_p_phase = C1 * (x[:,ε]*xx[ε,:] + y[:,ε]*yy[ε,:]) + \
+                  C2 * z[:,ε] * (xx**2 + yy**2)[ε,:] + \
+                  2*jnp.pi*pists[:,ε]
+
+    avg_field = jnp.vstack((
+        jnp.mean(jnp.cos(slm_p_phase), axis=0),
+        jnp.mean(jnp.sin(slm_p_phase), axis=0)
+    ))
+
+    return jnp.arctan2(avg_field[1], avg_field[0])
+
+
+IMPLS = [rs, rs_no_complex]
--- tests/contracts/test_impl_macro.py
+++ tests/contracts/test_impl_macro.py
@@ -1,5 +1,6 @@
 import slmporting.impl.numpy as impl_numpy
 import slmporting.impl.torch as impl_torch
+import slmporting.impl.jax   as impl_jax
 
 
 def test_numpy_impl():
@@ -9,4 +10,9 @@ def test_numpy_impl():
 
 def test_torch_impl():
     for implementation in impl_torch.IMPLS:
+        concrete = implementation()
+
+
+def test_jax_impl():
+    for implementation in impl_jax.IMPLS:
         concrete = implementation()
