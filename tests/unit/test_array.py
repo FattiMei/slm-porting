c30ccaa1235f410dbaab7ebd3d54561d08348bce
FattiMei <teomei68@gmail.com>
2025-10-05 16:55:46 +0200

    Add more complete test siute for array conversion, still misses the automatic detection of GPU backends

--- tests/unit/test_array.py
+++ tests/unit/test_array.py
@@ -4,11 +4,13 @@ import jax.numpy as jnp
 import torch
 import pytest
 
-from slmporting.core.array import Array, CachedArray, numpy_dtype_map
+from slmporting.core.array import Array, CachedArray, numpy_dtype_map, jax_dtype_map, torch_dtype_map, jax_device_map
 from slmporting.core.types import Backend, Device, DType
 
 
 all_backends = [Backend.NUMPY, Backend.JAX, Backend.TORCH]
+all_devices = [Device.CPU]
+# all_devices = [Device.CPU, Device.GPU] but check at runtime which device is supported
 all_dtypes = [DType.fp16, DType.fp32, DType.fp64]
 
 
@@ -19,23 +21,41 @@ backend_type_map = {
 }
 
 
-def ones(size: int, backend: Backend):
+def ones(size: int, backend: Backend, device: Device = Device.CPU, dtype: DType = DType.fp64):
     if backend == Backend.NUMPY:
-        return np.ones(size)
+        assert(device == Device.CPU)
+        return np.ones(
+            size,
+            dtype=numpy_dtype_map[dtype]
+        )
 
     elif backend == Backend.JAX:
-        return jnp.ones(size)
+        return jnp.ones(
+            size,
+            device=jax_device_map[device],
+            dtype=jax_dtype_map[dtype]
+        )
 
     elif backend == Backend.TORCH:
-        return torch.ones(size)
+        arr = torch.ones(
+            size,
+            dtype=torch_dtype_map[dtype]
+        )
+
+        if device == Device.GPU:
+            arr = arr.to('cuda')
+
+        return arr
 
     else:
         assert(False)
 
 
 @pytest.mark.parametrize("backend", all_backends)
-def test_array_creation(backend: Backend):
-    data = ones(100, backend)
+@pytest.mark.parametrize("device", all_devices)
+@pytest.mark.parametrize("dtype", all_dtypes)
+def test_array_creation(backend: Backend, device: Device, dtype: DType):
+    data = ones(100, backend, device, dtype)
     arr = Array(data)
 
 
@@ -43,7 +63,7 @@ def test_array_creation(backend: Backend):
 @pytest.mark.parametrize("dest", all_backends)
 @pytest.mark.parametrize("dtype", all_dtypes)
 def test_cpu_conversion(source: Backend, dest: Backend, dtype: DType):
-    data = ones(100, source)
+    data = ones(100, source, device = Device.CPU)
     arr = Array(data)
 
     converted = arr.convert_to(
@@ -54,6 +74,35 @@ def test_cpu_conversion(source: Backend, dest: Backend, dtype: DType):
 
     assert(isinstance(converted, backend_type_map[dest]))
 
+
+@pytest.mark.parametrize("source_backend", all_backends)
+@pytest.mark.parametrize("source_device",  all_devices)
+@pytest.mark.parametrize("source_dtype",   all_dtypes)
+@pytest.mark.parametrize("dest_backend", all_backends)
+@pytest.mark.parametrize("dest_device",  all_devices)
+@pytest.mark.parametrize("dest_dtype",   all_dtypes)
+def test_any_to_any_conversion(
+    source_backend: Backend,
+    source_device: Device,
+    source_dtype: DType,
+    dest_backend: Backend,
+    dest_device: Device,
+    dest_dtype: DType):
+
+    arr = Array(ones(
+        100, 
+        backend = source_backend,
+        device = source_device,
+        dtype = source_dtype
+    ))
+
+    converted = arr.convert_to(
+        backend = dest_backend,
+        device = dest_device,
+        dtype = dest_dtype
+    )
+
+    assert(isinstance(converted, backend_type_map[dest_backend]))
 
 
 @pytest.mark.parametrize("backend", all_backends)
@@ -85,10 +134,6 @@ def test_caching(backend: Backend, dtype: DType):
     assert(isinstance(second, backend_type_map[backend]))
 
 
-# The array module will require extensive tests. Those are only the surface
-# So far we haven't tested GPU transfers (because I don't have easy access
-# to a CUDA machine at the moment)
-#
 # We need to make sure that the transfers that are supposed to be zero copy
 # are in fact zero copy.
 #
