96863ba6839e0ffb1ef23c89d7c03803a5d85bb4
FattiMei <teomei68@gmail.com>
2025-10-08 17:21:13 +0200

    Add script for the verification of RS implementations

--- src/slmporting/core/contract.py
+++ src/slmporting/core/contract.py
@@ -1,4 +1,5 @@
 import time
+import torch
 import numpy as np
 import inspect
 import itertools
--- verification.py
+++ verification.py
@@ -4,9 +4,11 @@ import itertools
 import jax
 import jax.numpy as jnp
 jax.config.update('jax_enable_x64', True)
+import numpy as np
 
 from slmporting.core.slm import SLM
 from slmporting.core.array import Array
+from slmporting.core.types import Backend, Device, DType
 import slmporting.impl.jax
 import slmporting.impl.numpy
 import slmporting.impl.torch
@@ -54,6 +56,14 @@ def parse_command_line():
     return parser.parse_args()
 
 
+def error_function(x, y):
+    # we are dealing with a number that lives in [0, 2π]
+    # and wraps, so 2π-ε and ε are quite close
+    #
+    # for now we will use the standard error
+    return np.max(np.abs(x-y))
+
+
 if __name__ == '__main__':
     args = parse_command_line()
     nspots = args.nspots
@@ -70,17 +80,36 @@ if __name__ == '__main__':
 
     slm = SLM.get_standard_slm()
 
-    print('algorithm,name,backend,device,dtype,key,abs_err,rel_err')
+    print('algorithm,name,backend,device,dtype,key,abs_err,total_time')
 
     for n in range(args.nruns):
         # subkey is the key to export in the user report
         key, subkey = jax.random.split(key)
         subkeys = jax.random.split(subkey, 4)
 
+        # TODO: to suppress the jax alignment warnings we must reimplement
+        # the `AlignedArray` class
         x     = Array(jax.random.uniform(subkeys[0], nspots, minval=-50.0, maxval=50.0))
         y     = Array(jax.random.uniform(subkeys[1], nspots, minval=-50.0, maxval=50.0))
         z     = Array(jax.random.uniform(subkeys[2], nspots, minval=-5.0, maxval=5.0))
         pists = Array(jax.random.uniform(subkeys[3], nspots, minval=0.0, maxval=1.0))
 
-        # reference = all_implementations[0](x, y, z, pists, slm)
-        print(key_to_int(key))
+        reference_impl = all_implementations[0]
+        reference_phase, _ = reference_impl(x, y, z, pists, slm)
+        for impl in all_implementations:
+            assert(reference_impl.algorithm == impl.algorithm)
+            alternative_phase, times = impl(x, y, z, pists, slm)
+
+            error = error_function(reference_phase, alternative_phase)
+            total_time = times.transfer_time + times.compute_time
+
+            print(','.join(map(str, (
+                reference_impl.algorithm,
+                impl.name,
+                impl.backend,
+                Device.CPU,
+                DType.fp64,
+                key_to_int(key),
+                error,
+                total_time
+            ))))
