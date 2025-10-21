e054994efb0212db3527a96f2822495f4e034571
FattiMei <teomei68@gmail.com>
2025-10-21 21:31:10 +0200

    Add a robust way of importing C++ functions from python

--- slmporting/cpp/optimized.cpp
+++ slmporting/cpp/optimized.cpp
@@ -8,7 +8,7 @@ void scale_float(float* data, unsigned long n, float scale) {
 }
 
 
-void scale_inplace(torch::Tensor tensor, float scale) {
+void scale_inplace(torch::Tensor tensor, double scale) {
 	TORCH_CHECK(tensor.device().is_cpu(), "CPU tensor expected");
 	TORCH_CHECK(tensor.dtype() == torch::kFloat32, "float 32 expected");
 
@@ -18,6 +18,6 @@ void scale_inplace(torch::Tensor tensor, float scale) {
 }
 
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-	m.def("scale_inplace", &scale_inplace, "Scale tensor in-place");
+TORCH_LIBRARY(meilib, m) {
+	m.def("scale_inplace", &scale_inplace);
 }
--- slmporting/impl/impl_cpp.py
+++ slmporting/impl/impl_cpp.py
@@ -1,21 +1,18 @@
-import os
-from torch.utils.cpp_extension import load
 import torch
 
+try:
+    from slmporting import config
+except ImportError:
+    print("[ERROR]: can't find config file. Run a CMake build please")
+    exit(1)
 
 # this is a simple solution to make the c++ sources path
 # visible from this file
 #
 # otherwise when this file is included from someone/something else
 # the path to search the sources is the one of the includee
-src_dir = os.path.join(os.path.dirname(__file__), "..", "cpp")
-lib = load(
-    name="liboptimized",
-    sources=[os.path.join(src_dir, "optimized.cpp")],
-    extra_cflags=["-O3", "-march=native", "-ftree-vectorize", "-ffast-math", "-fopenmp"],
-    verbose=True
-)
+torch.ops.load_library(config.meilib_path)
 
 x = torch.arange(6, dtype=torch.float32)
-lib.scale_inplace(x, 2.0)
+torch.ops.meilib.scale_inplace(x, 2.0)
 print("scaled x:", x)
