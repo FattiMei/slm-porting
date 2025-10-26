0f16dcf7ecec72af4fd03694b86633a357e7c9c3
FattiMei <teomei68@gmail.com>
2025-10-26 09:17:35 +0100

    bugfix: fix the memory alignment of xx and yy arrays to suppress jax warnings

--- slmporting/core/slm.py
+++ slmporting/core/slm.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 from slmporting.core.types import DType
 from slmporting.core.array import CachedArray
@@ -22,11 +23,15 @@ class SLM:
         self.C1 = 2*np.pi / (wavelength.convert_to(Unit.MICROMETERS) * focal.convert_to(Unit.MICROMETERS))
         self.C2 =   np.pi / (wavelength.convert_to(Unit.MICROMETERS) * focal.convert_to(Unit.MICROMETERS)**2)
 
-        mesh = np.linspace(-1.0, 1.0, num=resolution)
-        xx, yy = np.meshgrid(mesh, mesh)
+        mesh = torch.linspace(-1.0, 1.0, resolution)
+        xx, yy = torch.meshgrid(mesh, mesh)
         mask = xx**2 + yy**2 < 1.0
 
-        self.pupil_idx = np.where(mask)
+        self.pupil_idx = torch.where(mask)
+
+        # these arrays need to be aligned so that jax won't complain
+        # if I use torch, they are automatically aligned, so no further
+        # processing is required
         self.xx = CachedArray(xx[mask] * pixel_size.convert_to(Unit.MICROMETERS) * resolution / 2.0)
         self.yy = CachedArray(yy[mask] * pixel_size.convert_to(Unit.MICROMETERS) * resolution / 2.0)
 
